f36f1931f9979c2773847117de1c7faf7372103b
Marcelo Fernandes <marcelofv12@hotmail.com>
2018-05-22 19:15:37 -0300

    correcao de alguns bugs e melhorias

--- scripts/character/player.gd
+++ scripts/character/player.gd
@@ -3,32 +3,13 @@ extends KinematicBody2D
 export (int) var move_speed = 900
 var dir = Vector2()
 
-var vida = 100
-var can_die = false
-var enemy
-
-signal punch
+var life = 100
+signal hit
 
 func _ready():
+	connect("hit", get_parent(), "hit")
 	add_to_group(general.GROUPS.player)
 
-func _physics_process(delta):
-	movement()
-	if Input.is_action_pressed("ui_select"):
-		emit_signal("punch")
-
-
-func _on_area_area_entered(area):
-	if area.get_parent().has_method("player_nearby"):
-		area.get_parent().player_nearby(self)
-		if !is_connected("punch", self, "enemy_enter"):
-			connect("punch", self, "enemy_enter")
-
-func enemy_enter():
-	if can_die:
-		if enemy.has_method("destroy"):
-			if !get_parent().empty_power(20):
-				enemy.destroy()
 
 func movement():
 	dir = Vector2()
@@ -43,22 +24,56 @@ func movement():
 	dir *= move_speed
 	move_and_slide(dir)
 
+func _physics_process(delta):
+	movement()
+	if Input.is_action_pressed("ui_select"):
+		damage(3)
+	
+	
+
+func damage(val = 1):
+	life -= val
+	if life <= 0:
+		emit_signal("hit", 0)
+	else:
+		emit_signal("hit", life)
+	
+	
+#var can_die = false
+#var enemy
+
+#signal punch
+
+#	if Input.is_action_pressed("ui_select"):
+#		emit_signal("punch")
+#
+#func _on_area_area_entered(area):
+#	if area.get_parent().has_method("player_nearby"):
+#		area.get_parent().player_nearby(self)
+#		if !is_connected("punch", self, "enemy_enter"):
+#			connect("punch", self, "enemy_enter")
+#
+#func enemy_enter():
+#	if can_die:
+#		if enemy.has_method("destroy"):
+#			if !get_parent().empty_power(20):
+#				enemy.destroy()
+
 #func set_power(power):
 #	if STATUS.power != STATUS.powers_available[power]:
 #		STATUS.power = STATUS.powers_available[power]
+#
+#func dead():
+#	if general.STATUS.life <= 0:
+#		queue_free()
+#
+#func hit(value):
+#	get_parent().hit(value)
 
-func dead():
-	if general.STATUS.life <= 0:
-		queue_free()
-
-
-func hit(value):
-	get_parent().hit(value)
-
-
-func _on_punchArea_area_entered(area):
-	can_die = true
-	enemy = area.get_parent()
-func _on_punchArea_area_exited(area):
-	can_die = false
-	enemy = area.get_parent()
+#
+#func _on_punchArea_area_entered(area):
+#	can_die = true
+#	enemy = area.get_parent()
+#func _on_punchArea_area_exited(area):
+#	can_die = false
+#	enemy = area.get_parent()
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,25 +1,32 @@
 extends Node
 
 var player_preload = preload("res://scenes/character/player.tscn")
-
+var player
 
 func _ready():
 	randomize()
 	spawn_player()
 
 func spawn_player():
-	var player = player_preload.instance()
-	player.position = $spawn.position
+	player = player_preload.instance()
+	player.position = $player_spaw.position
 	add_child(player)
 
-func empty_life(val):
-	if general.STATUS.vida <= 0:
-		return false
-	general.STATUS.vida -= val
-	$Control/life.scale = Vector2(float(general.STATUS.vida)/100, 1)
+func hit(_life):
+	$control/life.scale = Vector2(float(_life)/100, 1)
+	if _life == 0:
+		game_over()
 
-func empty_power(val):
-	if general.STATUS.power <= 0:
-		return true
-	general.STATUS.power -= val
-	$Control/energy.scale = Vector2(float(general.STATUS.power)/100, 1)
+func game_over():
+	print("game over")
+#func empty_life(val):
+#	if general.STATUS.vida <= 0:
+#		return false
+#	general.STATUS.vida -= val
+#	$Control/life.scale = Vector2(float(general.STATUS.vida)/100, 1)
+#
+#func empty_power(val):
+#	if general.STATUS.power <= 0:
+#		return true
+#	general.STATUS.power -= val
+#	$Control/energy.scale = Vector2(float(general.STATUS.power)/100, 1)
